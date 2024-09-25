b7d620d6a28275db11c3950b1fd1fbeab37dc676
sousukegit <greenneosoul.sousuke@gmail.com>
2024-09-25 09:56:03 +0900

    変数

---
+++ day3/sample3_2.php
@@ -0,0 +1,8 @@
+<?php
+    $hensu = 3;
+    echo "変数の値：{$hensu}<br>";
+    $hensu =  $hensu + 4;
+    echo "変数の値：{$hensu}<br>";
+    $hensu =  strval($hensu) . "そうすけ";
+    echo "変数の値：{$hensu}<br>";
+?>
